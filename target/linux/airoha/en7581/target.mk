9078bbbdb4025976b7406b2bb8ba975758316126
Lorenzo Bianconi <lorenzo@kernel.org>
2023-12-09 00:20:27 +0100

    airoha: add en7581 support (WIP)

---
+++ target/linux/airoha/en7581/target.mk
@@ -0,0 +1,9 @@
+ARCH:=aarch64
+SUBTARGET:=en7581
+BOARDNAME:=EN7581
+CPU_TYPE:=cortex-a53
+KERNELNAME:=Image dtbs
+
+define Target/Description
+	Build firmware images for Airhoa EN7581 ARM based boards.
+endef
