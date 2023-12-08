a6cd198fee0d7eae31b377899b15fba1b576c085
Lorenzo Bianconi <lorenzo@kernel.org>
2023-12-08 14:36:14 +0100

    airoha: move en7523 in a dedicated subtarget
    This is a preliminary patch to add AN7581 support

---
+++ target/linux/airoha/en7523/target.mk
@@ -0,0 +1,9 @@
+ARCH:=arm
+SUBTARGET:=en7523
+BOARDNAME:=EN7523
+CPU_TYPE:=cortex-a7
+KERNELNAME:=Image dtbs
+
+define Target/Description
+	Build firmware images for Airhoa EN7523 ARM based boards.
+endef
