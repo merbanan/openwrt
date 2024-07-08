be0d981612a589833816d428f0f0cbda5d1f51c3
Benjamin Larsson <benjamin.larsson@genexis.eu>
2024-07-08 22:47:04 +0200

    Add gpio polled button.
    Signed-off-by: Benjamin Larsson <benjamin.larsson@genexis.eu>

--- target/linux/airoha/image/en7581.mk
+++ target/linux/airoha/image/en7581.mk
@@ -1,7 +1,7 @@
 define Device/airoha_en7581-evb
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := EN7581 Evaluation Board (SNAND)
-  DEVICE_PACKAGES := kmod-leds-pwm kmod-i2c-en7581
+  DEVICE_PACKAGES := kmod-leds-pwm kmod-i2c-en7581 kmod-pwm-airoha kmod-input-gpio-keys-polled
   DEVICE_DTS := en7581-evb
   DEVICE_DTS_DIR := ../dts
 endef
