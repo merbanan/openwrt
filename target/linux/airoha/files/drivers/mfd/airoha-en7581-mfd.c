9ea2dbeef5ea9d00241886d283b9c8a83f153748
Lorenzo Bianconi <lorenzo@kernel.org>
2024-08-31 16:21:23 +0200

    airoha: mfd: align to upstream
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- target/linux/airoha/files/drivers/mfd/airoha-en7581-mfd.c
+++ target/linux/airoha/files/drivers/mfd/airoha-en7581-mfd.c
@@ -20,8 +20,7 @@ static const struct mfd_cell airoha_mfd_devs[] = {
 		.name = "pinctrl-airoha",
 		.resources = airoha_mfd_pinctrl_intr,
 		.num_resources = ARRAY_SIZE(airoha_mfd_pinctrl_intr),
-	},
-	{
+	}, {
 		.name = "airoha-pwm",
 	},
 };
@@ -35,6 +34,7 @@ static int airoha_mfd_probe(struct platform_device *pdev)
 	mfd = devm_kzalloc(dev, sizeof(*mfd), GFP_KERNEL);
 	if (!mfd)
 		return -ENOMEM;
+
 	platform_set_drvdata(pdev, mfd);
 
 	mfd->base = devm_platform_ioremap_resource(pdev, 0);
@@ -48,21 +48,21 @@ static int airoha_mfd_probe(struct platform_device *pdev)
 	airoha_mfd_pinctrl_intr[0].start = irq;
 	airoha_mfd_pinctrl_intr[0].end = irq;
 
-	return devm_mfd_add_devices(dev, PLATFORM_DEVID_AUTO,
-				    airoha_mfd_devs, ARRAY_SIZE(airoha_mfd_devs),
-				    NULL, 0, NULL);
+	return devm_mfd_add_devices(dev, PLATFORM_DEVID_AUTO, airoha_mfd_devs,
+				    ARRAY_SIZE(airoha_mfd_devs), NULL, 0,
+				    NULL);
 }
 
 static const struct of_device_id airoha_mfd_of_match[] = {
-	{ .compatible = "airoha,en7581-gpio-mfd", },
-	{},
+	{ .compatible = "airoha,en7581-gpio-sysctl" },
+	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, airoha_mfd_of_match);
 
 static struct platform_driver airoha_mfd_driver = {
-	.probe		= airoha_mfd_probe,
+	.probe = airoha_mfd_probe,
 	.driver = {
-		.name =	"en7581-mfd",
+		.name = KBUILD_MODNAME,
 		.of_match_table = airoha_mfd_of_match,
 	},
 };
